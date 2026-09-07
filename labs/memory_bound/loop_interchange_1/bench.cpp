1b30aab3875605217098193157b629ef5f4e5f22
Sergey Kintsel <serjonya@gmail.com>
2026-09-07 10:20:16 +0100

    Loop interchange 1 solved

--- labs/memory_bound/loop_interchange_1/bench.cpp
+++ labs/memory_bound/loop_interchange_1/bench.cpp
@@ -17,7 +17,19 @@ static void bench1(benchmark::State &state) {
 }
 
 // Register the function as a benchmark
-BENCHMARK(bench1);
+BENCHMARK(bench1)->Iterations(10);
+
+// baseline
+// ---------------------------------------------------------------
+// Benchmark                     Time             CPU   Iterations
+// ---------------------------------------------------------------
+// bench1/iterations:10  453680208 ns    453605586 ns           10
+
+// if doesn't touch res[i][j] on each iteration
+// ---------------------------------------------------------------
+// Benchmark                     Time             CPU   Iterations
+// ---------------------------------------------------------------
+// bench1/iterations:10  227535514 ns    227492183 ns           10
 
 // Run the benchmark
 BENCHMARK_MAIN();
