03f2eceb0691043fb668c10380a6618dcbd8feec
Sergey Kintsel <serjonya@gmail.com>
2026-09-08 18:51:32 +0100

    Lookup tables 1 solved

--- labs/bad_speculation/lookup_tables_1/solution.cpp
+++ labs/bad_speculation/lookup_tables_1/solution.cpp
@@ -1,15 +1,36 @@
 #include "solution.hpp"
 
-static std::size_t mapToBucket(std::size_t v) {
-                              //   size of a bucket
-  if      (v < 13)  return 0; //   13
-  else if (v < 29)  return 1; //   16
-  else if (v < 41)  return 2; //   12
-  else if (v < 53)  return 3; //   12
-  else if (v < 71)  return 4; //   18
-  else if (v < 83)  return 5; //   12
-  else if (v < 100) return 6; //   17
-  return DEFAULT_BUCKET;
+template <size_t N>
+static std::array<std::size_t, N> createLookupTable() {
+  std::array<std::size_t, N> arr;
+  for (std::size_t i{}; i < N; ++i) {
+    size_t x{};
+    if (i < 13)
+      x = 0;  //   13
+    else if (i < 29)
+      x = 1;  //   16
+    else if (i < 41)
+      x = 2;  //   12
+    else if (i < 53)
+      x = 3;  //   12
+    else if (i < 71)
+      x = 4;  //   18
+    else if (i < 83)
+      x = 5;  //   12
+    else if (i < 100)
+      x = 6;  //   17
+    else {
+      x = 7;
+    }
+    arr[i] = x;
+  }
+  return arr;
+}
+
+static auto lookupTable = createLookupTable<10000>();
+
+static std::size_t mapToBucket(const std::size_t v) {
+  return lookupTable[v];
 }
 
 std::array<std::size_t, NUM_BUCKETS> histogram(const std::vector<int> &values) {
