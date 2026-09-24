c5124abad8e1d1f0a37fe51a7a010d77eed6d48b
Sergey Kintsel <serjonya@gmail.com>
2026-09-24 18:23:54 +0100

    Solve dep chains 1

--- labs/core_bound/dep_chains_1/solution.cpp
+++ labs/core_bound/dep_chains_1/solution.cpp
@@ -1,6 +1,7 @@
 #include "solution.hpp"
 #include <array>
 #include <iostream>
+#include <print>
 
 unsigned getSumOfDigits(unsigned n) {
   unsigned sum = 0;
@@ -11,6 +12,61 @@ unsigned getSumOfDigits(unsigned n) {
   return sum;
 }
 
+// baseline
+// -----------------------------------------------------
+// Benchmark           Time             CPU   Iterations
+// -----------------------------------------------------
+// bench1           56.6 ms         56.6 ms           11
+
+// split into two parallel checks
+// while (l2) {
+// if (i < len - SIZE) {
+//   i += 2;
+//   unsigned a = (l2++)->value;
+//   unsigned b = (l2++)->value;
+//   if (a == v || b == v) {
+//     retVal += getSumOfDigits(v);
+//     break;
+//   }
+// } else {
+//   if (l2->value == v) {
+//     retVal += getSumOfDigits(v);
+//     break;
+//   }
+//   l2 = l2->next;
+// }
+// -----------------------------------------------------
+// Benchmark           Time             CPU   Iterations
+// -----------------------------------------------------
+// bench1           14.6 ms         14.6 ms           39
+
+// with 3
+// -----------------------------------------------------
+// Benchmark           Time             CPU   Iterations
+// -----------------------------------------------------
+// bench1           12.2 ms         12.2 ms           48
+
+// with 4
+// -----------------------------------------------------
+// Benchmark           Time             CPU   Iterations
+// -----------------------------------------------------
+// bench1           11.1 ms         11.1 ms           57
+
+// with array of Size 4
+// -----------------------------------------------------
+// Benchmark           Time             CPU   Iterations
+// -----------------------------------------------------
+// bench1           11.0 ms         11.0 ms           58
+
+// higher size it just degrades
+
+
+// with proper bucketing and size = 8
+// -----------------------------------------------------
+// Benchmark           Time             CPU   Iterations
+// -----------------------------------------------------
+// bench1           12.6 ms         12.6 ms           49
+
 // Task: lookup all the values from l2 in l1.
 // For every found value, find the sum of its digits.
 // Return the sum of all digits in every found number.
@@ -20,22 +76,33 @@ unsigned getSumOfDigits(unsigned n) {
 // Hint: Traversing a linked list is a long data dependency chain:
 //       to get the node N+1 you need to retrieve the node N first.
 //       Think how you can execute multiple dependency chains in parallel.
+constexpr size_t SIZE = 4;
 unsigned solution(List *l1, List *l2) {
   unsigned retVal = 0;
 
   List *head2 = l2;
+
   // O(N^2) algorithm:
   while (l1) {
-    unsigned v = l1->value;
+    std::array<unsigned, SIZE> values;
+    size_t len{};
+    while (l1 && len < SIZE) {
+      values[len++] = l1->value;
+      l1 = l1->next;
+    }
+
     l2 = head2;
-    while (l2) {
-      if (l2->value == v) {
-        retVal += getSumOfDigits(v);
-        break;
+    size_t found{};
+    while (l2 && found < len) {
+      for (size_t j{}; j < len; ++j) {
+        if (l2->value == values[j]) {
+          ++found;
+          retVal += getSumOfDigits(values[j]);
+          break;
+        }
       }
       l2 = l2->next;
     }
-    l1 = l1->next;
   }
 
   return retVal;
